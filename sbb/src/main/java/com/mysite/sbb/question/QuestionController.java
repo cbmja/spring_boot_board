25ce2bf0582ec878bd2b6c1406e297cba0cc1fed
cbmja <cbmja@naver.com>
2024-03-08 20:33:54 +0900

    ,

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -35,6 +37,25 @@ public class QuestionController {
 		model.addAttribute("question",question);
 		return "question_detail";
 	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "question_form";
+	}
+	
+	
+	@PostMapping("/create")
+	public String questCreate(@RequestParam(name = "subject") String subject ,
+							  @RequestParam(name = "content") String content) {
+		
+		questionService.create(subject, content);
+		
+		return "redirect:/question/list";
+	}
+	
+	
+	
+	
 	
 	
 }

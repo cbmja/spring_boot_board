6fd316a5228f77282082e9922159344911b2b89b
cbmja <cbmja@naver.com>
2024-03-09 13:15:02 +0900

    d

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,11 +1,14 @@
 package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
@@ -19,8 +22,11 @@ public class QuestionService {
 	private final QuestionRepository questionRepository;
 	
 	public Page<Question> getList( int page){
-											
-		Pageable pageable = PageRequest.of(page, 10); //현재 페이지 / 페이지당 게시글 수
+					
+		List<Sort.Order> sorts = new ArrayList<>();
+		sorts.add(Sort.Order.desc("createDate")); //내림차순(최신순 정렬) - 셀렉트박스로 과거순 선택 가능하게 분기하면 될듯
+		
+		Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); //현재 페이지 / 페이지당 게시글 수
 		
 		return questionRepository.findAll(pageable);
 	}
